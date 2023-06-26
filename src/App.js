a45ac915e22f6d6314dbe91cdbbbac5b601c5afe
Rehmann Tyagi <irontyagi4@gmail.com>
2023-06-27 02:58:35 +0530

    deleted formtask

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import { BsReceipt } from 'react-icons/bs'
 import AppHeader from "./components/header/header.component";
 import ProgressBar from "./components/progressBar/progressBar.component";
 import TaskContainer from "./components/taskContainer/taskContainer.component";
-import TaskForm from "./components/taskForm/taskForm.component";
 import tasks from "./tasks";
 
 function App() {
